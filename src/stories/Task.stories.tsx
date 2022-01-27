6ccf33aef74f4a563e2dfde34d33fe8e1ddbbf7d
DavidPoshelyk <davidposhelyk@gmail.com>
2022-01-27 13:28:35 +0300

    test integration

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Task } from '../Task';
+import {AddItemForm} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
+import {Task} from "../Task";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'TODOLISTS/Task',
-    component: Task,
+  title: 'TodoList/Task',
+  component: Task,
     args:{
         changeTaskStatus: action('changeTaskStatus'),
         changeTaskTitle: action('changeTaskTitle'),
         removeTask: action('removeTask'),
-        todolistId: '12'
+        todolistId: '1234'
     }
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
-export const TaskStoryisDone = Template.bind({});
-TaskStoryisDone.args = {
-    task: {id:'2134', title:'hi storybook', isDone: true}
-}
-
-export const TaskStoryNotisDone = Template.bind({});
-TaskStoryNotisDone.args = {
-    task: {id:'2134', title:'hi storybook', isDone: false}
-}
-
+export const TaskStoriesFalse = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskStoriesFalse.args = {
+ task:{id:'jhg', title:'ho storybook', isDone:false}
+};
+
+export const TaskStoriesTrue = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskStoriesTrue.args = {
+    task:{id:'jhg', title:'ho storybook', isDone:true}
+};
 
